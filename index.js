71634d96ce0d8fd619ea88b2ce37654f4ddd2edc
Br0ke138 <br0ke138@gmail.com>
2019-04-08 22:54:38 +0200

    fix

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api');
 const Coss = require('coss-api-node');
 
+// ################# CONFIG ###################
+// ################# CONFIG ###################
+
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'BOT_TOKEN';
+const token = 'bot_token';
 const bot = new TelegramBot(token, {polling: true});
 
 // How often should the bot check for order updates
-// Try not to tickle the Rate Limit. It gets angry fast.
 const CHECK_RATE_IN_SECONDS = 60;
 
 // FOR MULTIPLE PAIRS
-const TIME_BETWEEN_CALLS_IN_SECONDS = 5
+const TIME_BETWEEN_CALLS_IN_SECONDS = 5;
 
 // Start bot and use /start to get your chat id
-const CHAT_ID = 1234567;
+const CHAT_ID = 'CHAT_ID';
 
 // COSS API KEYS
-const PUBLIC_KEY = 'PUBLIC_KEY';
-const PRIVATE_KEY = 'PRIVATE_KEY';
+const PUBLIC_KEY = 'Your_Public_Key';
+const PRIVATE_KEY = 'Your_Private_Key';
 
 let pairs = [];
 
 // A restart of the bot loses all added pairs which you added via /add
 // Uncomment to add COSS-ETH to the watch list everytime the script gets started
-// pairs = ['COSS-ETH']
+// pairs = ['COSS-ETH'];
 
+// ################# CONFIG ###################
+// ################# CONFIG ###################
 
 let interval;
 let coss = new Coss({
@@ -32,8 +36,8 @@ let coss = new Coss({
     private_api_key: PRIVATE_KEY,
 });
 
-let filled = {};
-let partial = {};
+// Open Orders + Partial Filled open orders
+let open = {};
 
 
 bot.onText(/\/start/, (msg) => {
@@ -57,48 +61,47 @@ bot.onText(/\/commands/, (msg) => {
             bot.sendMessage(chatId, 'MIEP MÖP. Thats not your bot (wrong chatId)');
         }
     } else {
-        bot.sendMessage(chatId, 'Set your chatId in the bot code first\nchatId: ' + chatId);
+        bot.sendMessage(chatId, 'Set your chatId in the bot code first');
     }
 });
-
 
 bot.onText(/\/watch/, (msg) => {
     const chatId = msg.chat.id;
     if (CHAT_ID === chatId) {
         if (pairs.length < 1) {
-            bot.sendMessage(chatId, "Use /add first to add a pair to watch");
+            bot.sendMessage(CHAT_ID, "Use /add first to add a pair to watch");
         } else {
-            startBot(chatId);
+            startBot();
         }
     }
-})
+});
 
 bot.onText(/\/stop/, (msg) => {
     const chatId = msg.chat.id;
     if (CHAT_ID === chatId) {
-        stopBot(chatId);
+        stopBot();
     }
-})
+});
 
 bot.onText(/\/add/, (msg) => {
     const chatId = msg.chat.id;
     if (CHAT_ID === chatId) {
-        askForPair(chatId, function (pair) {
+        askForPair( function (pair) {
             if (pairs.includes(pair)) {
-                bot.sendMessage(chatId, "Already watching " + pair);
+                bot.sendMessage(CHAT_ID, "Already watching " + pair);
             } else if (pair.includes('/') || !pair.includes('-') || pair.includes('_')) {
-                bot.sendMessage(chatId, "Format: COSS-ETH");
-                bot.sendMessage(chatId, "Use /add again");
+                bot.sendMessage(CHAT_ID, "Format: COSS-ETH");
+                bot.sendMessage(CHAT_ID, "Use /add again");
             } else {
                 if (interval) {
-                    bot.sendMessage(chatId, "Collecting previous completed and partial filled orders");
-                    checkOrders(chatId, pair.toUpperCase(), () => {
+                    bot.sendMessage(CHAT_ID, "Collecting previous completed and partial filled orders");
+                    checkOrders( pair.toUpperCase(), () => {
                         pairs.push(pair.toUpperCase());
-                        bot.sendMessage(chatId, "Bot started watching " + pair.toUpperCase());
+                        bot.sendMessage(CHAT_ID, "Bot started watching " + pair.toUpperCase());
                     })
                 } else {
                     pairs.push(pair.toUpperCase());
-                    bot.sendMessage(chatId, pair.toUpperCase() + " added to the list. Start the bot with /watch");
+                    bot.sendMessage(CHAT_ID, pair.toUpperCase() + " added to the list. Start the bot with /watch");
                 }
             }
         })
@@ -108,14 +111,14 @@ bot.onText(/\/add/, (msg) => {
 bot.onText(/\/remove/, (msg) => {
     const chatId = msg.chat.id;
     if (CHAT_ID === chatId) {
-        askForPair(chatId, function (pair) {
+        askForPair(function (pair) {
             if (!pairs.includes(pair)) {
-                bot.sendMessage(chatId, "Not watching " + pair);
+                bot.sendMessage(CHAT_ID, "Not watching " + pair);
             } else {
                 pairs = pairs.filter(function (item) {
                     return item !== pair.toUpperCase();
-                })
-                bot.sendMessage(chatId, "Bot stopped watching " + pair.toUpperCase());
+                });
+                bot.sendMessage(CHAT_ID, "Bot stopped watching " + pair.toUpperCase());
             }
         })
     }
@@ -125,114 +128,129 @@ bot.onText(/\/list/, (msg) => {
     const chatId = msg.chat.id;
     if (CHAT_ID === chatId) {
         if (pairs.length < 1) {
-            bot.sendMessage(chatId, "List is empty. Add some pairs with /add");
+            bot.sendMessage(CHAT_ID, "List is empty. Add some pairs with /add");
         } else {
-            bot.sendMessage(chatId, pairs.toString());
+            bot.sendMessage(CHAT_ID, pairs.toString());
         }
     }
-})
+});
 
-function askForPair(chatId, callback) {
-    bot.sendMessage(chatId, "Insert Pair").then(() => {
+function askForPair(cb) {
+    bot.sendMessage(CHAT_ID, "Insert Pair").then(() => {
         bot.once('message', (pair) => {
-            callback(pair.text);
+            cb(pair.text);
         })
     })
 }
 
-function startBot(chatId) {
+function startBot() {
     if (interval) {
         clearInterval(interval);
     }
 
-    bot.sendMessage(chatId, "Collecting previous completed and partial filled orders");
-    let counter = 0;
-    pairs.forEach((pair) => {
-        setTimeout(() => {
-            savePrevious(pair, () => {
-                bot.sendMessage(chatId, "Orders for " + pair + " collected.");
-                counter++;
-                if (counter === pairs.length) {
-                    interval = setInterval(() => {
-                        getOrderHistory(chatId);
-                    }, CHECK_RATE_IN_SECONDS * 1000);
-                    bot.sendMessage(chatId, "Bot started");
-                }
-            });
-        }, TIME_BETWEEN_CALLS_IN_SECONDS * 1000)
-    })
+    bot.sendMessage(CHAT_ID, "Collecting orders ...");
+    checkForUpdates();
+    bot.sendMessage(CHAT_ID, "Bot started");
+
+    interval = setInterval(() => {
+        checkForUpdates();
+    }, CHECK_RATE_IN_SECONDS * 1000);
 }
 
-function stopBot(chatId) {
+function stopBot() {
     if (interval) {
         clearInterval(interval);
-        bot.sendMessage(chatId, "Bot stopped");
+        bot.sendMessage(CHAT_ID, "Bot stopped");
     } else {
-        bot.sendMessage(chatId, "Bot not running");
+        bot.sendMessage(CHAT_ID, "Bot not running");
     }
 }
 
-function getOrderHistory(chatId) {
+function checkForUpdates() {
     if (pairs.length < 1) {
-        bot.sendMessage(chatId, "No pairs to watch. Add some with /add");
+        bot.sendMessage(CHAT_ID, "No pairs to watch. Add some with /add");
     } else {
-        pairs.forEach(pair => {
-            setTimeout(() => {
-                checkOrders(chatId, pair);
-            }, TIME_BETWEEN_CALLS_IN_SECONDS * 1000);
-        })
+        processPair(pairs, 0);
     }
 }
 
-async function checkOrders(chatId, pair, cb = () => {}) {
-    try {
-        const completedOrders = await coss.getCompletedOrders({Limit: 100000, Symbol: pair})
-        completedOrders.list.forEach(order => {
-            if (order.status === 'filled') {
-                if (!filled[order.order_id]) {
-                    const msg =
-                        'Pair: ' + order.order_symbol + '\n' +
-                        'Side: ' + order.order_side + '\n' +
-                        'Status: ' + order.status + '\n' +
-                        'Amount: ' + order.executed;
-                    bot.sendMessage(chatId, msg);
-                    filled[order.order_id] = true;
+function processPair(pairs, index) {
+    const pair = pairs[index];
+
+    setTimeout(() => {
+        processNewAndUpdatedOpenOrders(pair, (err) => {
+            if (err) {
+                console.log('Unable to fetch open Orders', err);
+            }
+            processCompletedOrders(pair, (err) => {
+                if (err) {
+                    console.log('Unable to fetch completed Orders', err);
                 }
-            } else if (order.status === 'partial_fill') {
-                // Order not seen or executed amount updated
-                if (!partial[order.order_id] || partial[order.order_id] !== order.executed) {
+                if (pairs[index + 1]) {
+                    processPair(pairs, index + 1);
+                }
+            })
+        });
+    }, TIME_BETWEEN_CALLS_IN_SECONDS * 1000);
+}
+
+function processNewAndUpdatedOpenOrders(pair, cb) {
+    coss.getOpenOrders(pair, 1000, 0, (err, resp, orders) => {
+        if (err) {
+            cb(err);
+        } else {
+            orders.forEach((order) => {
+                if (open[order.order_id] && open[order.order_id] !== order.executed) {
                     const msg =
+                        'Order update:' + '\n' +
                         'Pair: ' + order.order_symbol + '\n' +
                         'Side: ' + order.order_side + '\n' +
                         'Status: ' + order.status + '\n' +
                         'Progress: ' + order.executed + '/' + order.order_size + '(' + (order.executed * 100 / order.order_size).toFixed(0) + '%)';
-                    bot.sendMessage(chatId, msg);
-                    partial[order.order_id] = order.executed;
+                    bot.sendMessage(CHAT_ID, msg);
+                    open[order.order_id] = order.executed;
+                } else {
+                    const msg =
+                        'New Order:' + '\n' +
+                        'Pair: ' + order.order_symbol + '\n' +
+                        'Side: ' + order.order_side + '\n' +
+                        'Status: ' + order.status + '\n' +
+                        'Progress: ' + order.executed + '/' + order.order_size + '(' + (order.executed * 100 / order.order_size).toFixed(0) + '%)';
+                    bot.sendMessage(CHAT_ID, msg);
+                    open[order.order_id] = order.executed;
                 }
-            }
-        })
-        cb();
-
-    } catch (e) {
-        // bot.sendMessage(chatId, e);
-        console.log(e);
-    }
+            });
+            cb(null);
+        }
+    });
 }
 
-async function savePrevious(pair, cb) {
-    try {
-        const completedOrders = await coss.getCompletedOrders({Limit: 100000, Symbol: pair})
-        completedOrders.list.forEach(order => {
-            if (order.status === 'filled') {
-                filled[order.order_id] = true;
-            } else if (order.status === 'partial_fill') {
-                partial[order.order_id] = order.executed;
-            }
-        })
-        cb();
-
-    } catch (e) {
-        // bot.sendMessage(chatId, e);
-        console.log(e);
-    }
+function processCompletedOrders(pair, cb) {
+    coss.getCompletedAndCancelledOrders(pair, 2147483647, 0, (err, resp, orders) => {
+        if (err) {
+            cb(err);
+        } else {
+            orders.forEach((order) => {
+                if (open[order.order_id] && order.status === 'filled') {
+                    const msg =
+                        'Order filled:' + '\n' +
+                        'Pair: ' + order.order_symbol + '\n' +
+                        'Side: ' + order.order_side + '\n' +
+                        'Status: ' + order.status + '\n' +
+                        'Progress: ' + order.executed + '/' + order.order_size + '(' + (order.executed * 100 / order.order_size).toFixed(0) + '%)';
+                    bot.sendMessage(CHAT_ID, msg);
+                } else {
+                    const msg =
+                        'Order canceled:' + '\n' +
+                        'Pair: ' + order.order_symbol + '\n' +
+                        'Side: ' + order.order_side + '\n' +
+                        'Status: ' + order.status + '\n' +
+                        'Progress: ' + order.executed + '/' + order.order_size + '(' + (order.executed * 100 / order.order_size).toFixed(0) + '%)';
+                    bot.sendMessage(CHAT_ID, msg);
+                }
+                delete open[order.order_id];
+            });
+            cb(null);
+        }
+    })
 }
